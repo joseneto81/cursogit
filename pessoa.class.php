e38516a961f123d1b153f30f24a1c298318635fd
Jose Neto <jose.neto@riodasostras.rj.gov.br>
2015-10-16 16:35:55 -0300

    Adicionadas duas funcoes na classe

--- pessoa.class.php
+++ pessoa.class.php
@@ -16,11 +16,23 @@ class Pessoa
 	
 	public function get($propetry)
 	{
-		return $propetry;
+		return $this->$propetry;
+	}
+
+	public function getNome()
+        {
+        	return $this->nome;
+        }
+
+	public function getIdade()
+	{
+	return round((date("Ymd")-str_replace("-","",$this->nasc))/10000)." anos";
 	}
 }
 
 $pessoa = new Pessoa();
 $pessoa->set("nome","Joao");
-print $pessoa->get("Nome");
+print $pessoa->get("nome")."\n";
+$pessoa->set("nasc","1981-01-01");
+print $pessoa->getIdade()."\n";
 ?>
